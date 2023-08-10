6f21ff003b35ecfbb3ed1ca91293533242e388b5
Janella-codes <118861074+Janella-codes@users.noreply.github.com>
2023-08-09 17:07:10 -0700

    Update nuxt.config.ts

--- my-app/nuxt.config.ts
+++ my-app/nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
 
-})
+export default defineNuxtConfig({
+    $production: {
+      routeRules: {
+        '/**': { isr: true }
+      }
+    },
+    $development: {
+      //
+    }
+  })
+
+function defineNuxtConfig(arg0: { $production: { routeRules: { '/**': { isr: boolean; }; }; }; $development: {}; }) {
+    throw new Error("Function not implemented.");
+}
+
+
